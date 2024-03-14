f876fcd2412a8b69fe022afa379d91a7f47af9d3
buriniyozovs <sburiniyozov@gmail.com>
2024-03-14 21:31:03 +0500

    Initialized project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const config = require('config');
+const mongoose = require('mongoose');
+
+require('./startup/db')();
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{console.log(`Listening in port ${port}`)});
